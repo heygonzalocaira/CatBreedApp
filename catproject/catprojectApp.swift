dd196c62b822c1912eac4455aaebca27d35641a0
Gonzalo Caira <gcairaaguilar@gmail.com>
2023-05-06 13:52:04 -0500

    Initial Commit

---
+++ catproject/catprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  catprojectApp.swift
+//  catproject
+//
+//  Created by heygonza on 6/05/23.
+//
+
+import SwiftUI
+
+@main
+struct catprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
